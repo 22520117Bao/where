2a53a22fa3e29130c09dc21d38dc78fae63e43fd
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-03-18 21:34:05 +0700

    Add files via upload

---
+++ bai11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace bai11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pi= "+ Math.PI);
+            Console.WriteLine("kieu du lieu cau math.pi = "+ Math.PI.GetType().ToString());
+            Console.ReadKey();
+        }
+        
+    }
+}
