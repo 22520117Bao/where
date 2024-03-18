e975ae767627122ccbb3174f1aba41aa7cedd418
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-03-18 21:38:41 +0700

    Add files via upload

---
+++ bai10/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace bai10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vong lap for
+            // int tong = 0;
+            //for (int i = 0; i<= 10;i++)
+            // {
+            //     tong += i;
+
+            //     Console.WriteLine(" tong la:  " + tong);
+
+            // }
+
+            // foreach (<kieu du lieu><bien lap> in <ten mang or tap hop> )
+            //{
+            //<khoi lenh>
+            //}
+            //int[] lst = { 1, 3, 5, 7, 8, 10, 12 };// hkoi tao 1 list
+            //int thang;
+            //Console.WriteLine(  "nhap thang ");
+            //thang = int.Parse(Console.ReadLine());
+            //foreach (int i in lst)
+            //{
+            //    Console.WriteLine("i = "+ i);
+            //    if (thang == i)
+            //    {
+            //        Console.WriteLine("thang cua {0} co 31 ngay" ,i);
+            //        break;
+            //    }
+            //}
+            //string[] lst = { "lmao", "dark", "bruh" };
+            //foreach(string s in lst)
+            //{
+            //    Console.WriteLine("ta co meme  "+ s );
+            //}
+
+            // continue  && break
+            //int tong = 0;
+            //for (int i = 0;i<=5;i++)
+            //{
+            //    if (i == 3)
+            //        break;
+            //    else
+            //    {
+            //        Console.WriteLine(  "i = " + i);
+            //        tong = tong + i;
+            //    }
+            //}
+            //Console.WriteLine("tong la: "+ tong);
+
+            // bai tap giai thua
+
+            //int n;
+            //int m = 1;
+            //Console.WriteLine("hay nhap 1 so:  ");
+            //n = int.Parse(Console.ReadLine());  
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (n == 0)
+            //    {
+            //        m = 1; break;
+            //    }
+            //    else
+            //    {
+            //        if (i  == 0)
+            //        {
+            //            continue;
+
+            //        }
+            //        else
+            //        {
+            //            m = m * i;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("so giai thua cua so {0} la {1}", n, m);
+            //Tong cac so chan
+            //int n;
+            //Console.WriteLine("nhap n: ");
+            //n=  int.Parse(Console.ReadLine());
+            //int tong = 0;
+            //for (int i = 0; i<=n; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        tong += i;
+            //    else
+            //        Console.WriteLine("toi ko tinh so le bye bye hehe");
+
+            //}
+            //Console.WriteLine(" tong la: " + tong);
+
+            // tong so le bo so 3
+            //int n;
+            //Console.WriteLine("nhap n: ");
+            //n = int.Parse(Console.ReadLine());
+            //int tong = 0;
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        if (i != 3)
+            //            tong += i;
+            //        else continue;
+            //    }
+
+            //}
+            //Console.WriteLine(" tong la: " + tong);
+
+            // so chia het cho 3
+            //for (int i  = 10; i<=50;i++)
+            //{
+            //    if(i%3 == 0)
+            //        Console.WriteLine(  "so chia het cho 3 i = "+ i);
+            //}
+
+            // tong giai thua tu 1! +...+ 10!
+            //int tong = 1;
+            //int tun = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+
+            //        tong = tong * i;
+            //        tun = tun + tong;
+
+
+            //}
+            //Console.WriteLine("tong la:  "+ tun);
+
+            // so hoan hao
+            //int n;
+            //Console.WriteLine("Nhap n: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //for (int j = 1; j <= 1000; j++)
+            //{
+            //    int tong = 0;
+
+
+            //    for (int i = 1; i < j; i++)
+            //    {
+            //        if (j % i == 0)
+            //        {
+            //            tong = tong + i;
+            //        }
+
+            //    }
+            //    if (tong == j)
+            //        Console.WriteLine("{0} la so hoan hao ", j);
+            //}
+            // so nguyen to
+
+            //int a;
+            //int uoc =0;
+            //Console.WriteLine("nhap so nguyen a: ");
+            //a =int.Parse(Console.ReadLine());
+            //while(a<= 0)
+            //{
+            //    Console.WriteLine("moi nhap lai a>0 ");
+            //    a = int.Parse(Console.ReadLine());
+            //}
+            //for (int i  = 1; i<= a; i++)
+            //{
+            //    if (a % i == 0)
+            //        uoc++;
+            //}
+            //if (uoc == 2)
+            //    Console.WriteLine("{0} la so nguyen to ", a);
+            //else
+            //    Console.WriteLine("{0} ko la so nguyen to ", a);
+
+            //Console.ReadKey();
+        }
+    }
+}
